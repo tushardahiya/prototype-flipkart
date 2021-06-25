df8b88e0e28d2144910a212e9681ba6f7fa204df
tushardahiya <tushardahiya84@gmail.com>
2021-06-25 14:58:00 +0530

    fixed ui and product cards

--- src/Components/Products/Product/Product.js
+++ src/Components/Products/Product/Product.js
@@ -4,18 +4,18 @@ import "./Product.css";
 const Product = ({ details }) => {
   return (
     <div className="product-container">
-      <div className="title">
-        <small>{details.Brand}</small>
-        <p>{details.Title}</p>
-      </div>
-
       <img src={details.img} alt="product" />
-
       <p className="description">{details.Description}</p>
+      <div className="title">
+        <small>Brand : {details.Brand}</small>
+        <small>Type : {details.Title}</small>
+      </div>
       <div className="product-info">
         <h6>Size : {details.Size} </h6>
         <h6>Sex : {details.Gender}</h6>
-        <h6> Price : {details.Price}</h6>
+      </div>
+      <div className="price-container">
+        <p> Price : {details.Price}</p>
       </div>
     </div>
   );
